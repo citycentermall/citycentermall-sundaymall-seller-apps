28e7fdf9f1fabc81600f6da8e00940036d019170
masum-raja728 <masumansariedugaon@gmail.com>
2025-05-04 10:09:05 +0530

    Customized with updated branding, metadata, and structure Replaced default app icons and assets with custom branding in assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:sunday_mall/views/screens/onboarding/onboading_screen.dart';
+import 'package:shop/route/route_constants.dart';
+import 'package:shop/route/router.dart' as router;
+import 'package:shop/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-/// test time
+// Thanks for using our template. You are using the free version of the template.
+// 🔗 Full template: https://theflutterway.gumroad.com/l/fluttershop
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnboardingScreen(),
+      title: 'Shop Template by The Flutter Way',
+      theme: AppTheme.lightTheme(context),
+      // Dark theme is inclided in the Full template
+      themeMode: ThemeMode.light,
+      onGenerateRoute: router.generateRoute,
+      initialRoute: onbordingScreenRoute,
     );
   }
 }
-
